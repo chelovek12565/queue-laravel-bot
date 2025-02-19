6c73dc098c6951b1a22e086ae41a3840876df1dd
chel <bas180407@mail.ru>
2025-02-19 18:54:21 +0300

    queue migration added

---
+++ queue-app/database/migrations/2025_02_19_153937_create_queue_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up() {
+        Schema::create('queues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('room_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('queue_user', function (Blueprint $table) {
+            $table->foreignId('queue_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('position')->default(0);
+            $table->primary(['queue_id', 'user_id']);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queues');
+        Schema::dropIfExists('queue_user');
+    }
+};
