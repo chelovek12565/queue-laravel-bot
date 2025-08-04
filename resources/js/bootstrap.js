b916748ac0108c241d19e709e7abb5b7c7581b37
chelovek12565 <bas180407@mail.ru>
2025-08-04 19:11:13 +0300

    fix for tabs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,6 +3,9 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+// Import Bootstrap JavaScript
+import * as bootstrap from 'bootstrap';
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
