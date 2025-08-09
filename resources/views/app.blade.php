9514a1afb38c73eabb1190c4e8a2035d3783cb68
chelovek12565 <bas180407@mail.ru>
2025-08-10 00:11:52 +0300

    added livewire styles and scripts to app.blade,php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @livewireStyles
     <title>@yield('title', 'My Laravel App')</title>
 
     <script src="https://telegram.org/js/telegram-web-app.js?57"></script>
@@ -32,5 +33,6 @@
     </footer>
 
     @yield('scripts')
+    @livewireScripts
 </body>
 </html>
